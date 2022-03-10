39008923223b8f5f1f94532ef58e96f80a3e2fbe
yk0819 <94211862+yk0819@users.noreply.github.com>
2022-03-10 10:59:49 +0900

    Update AcceptThreadChat.java

--- AcceptThreadChat.java
+++ AcceptThreadChat.java
@@ -19,9 +19,11 @@ public class AcceptThreadChat implements Runnable {
 	    ServerSocket AcceptThread = null;
 	    Socket socket = null;
 	    String userName = null;
-	    String answer = null;
 	    Scanner userNameScanner = null;
-	    Scanner userNameAnswer = null;
+	    Scanner text = new Scanner(System.in);
+	    BufferedReader in = null;
+	    BufferedWriter out = null;
+	
 	    
 		try {
 			AcceptThread = new ServerSocket(this.port); // 서버소켓 설정 생성자값
@@ -32,6 +34,29 @@ public class AcceptThreadChat implements Runnable {
 				System.out.println("연결완료되었습니다");
 				InetSocketAddress isa = (InetSocketAddress)socket.getRemoteSocketAddress(); // 상대방 정보 가져오기
 				System.out.println("IP주소: "+isa.getHostName()+", port: "+socket.getLocalPort()+"와 연결이 되었습니다");
+				
+				// 닉네임 설정
+				userNameScanner = new Scanner(System.in);
+				System.out.print("닉네임을 입력해주세요: ");
+				userName = userNameScanner.nextLine();
+				System.out.println("닉네임을" + userName + "(으)로 지정하였습니다");
+				
+				System.out.println(userName+"님이 입장하셨습니다");
+				
+				in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+				
+				while(true) {	
+					// 메세지 보내기
+					System.out.println(">");
+					String outMessage = text.nextLine();
+					out.write(outMessage+"\n");
+					out.flush();
+					
+					String inMessage = in.readLine();
+					System.out.println(userName+": "+inMessage);
+				}
+
 			}
 		}catch(Exception e) {
 			System.out.println("연결이 종료되었습니다.");
